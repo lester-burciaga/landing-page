4521fe89d7c4c8203c90139f3d11e514d00ff925
lesterbv06 <bloodshot_x@hotmail.com>
2024-07-05 09:45:24 -0600

    creation of pages & components folder structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from '@/components/login/Login';
+
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <Login />
+        </div>
+    );
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/navbar/Navbar'
+
+export default function ProfilePage() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Profile</h1>
+        </div>
+    );
+}
