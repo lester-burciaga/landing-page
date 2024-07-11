512552b3986ef7efe0fe16c5d8cde8f138cab104
lesterbv06 <bloodshot_x@hotmail.com>
2024-07-11 10:13:37 -0600

    log out test added on navbar test file

--- src/components/organisms/navbar/Navbar.test.tsx
+++ src/components/organisms/navbar/Navbar.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import Navbar from './Navbar';
 
@@ -14,7 +14,13 @@ jest.mock("next/navigation", () => {
     }
 });
 
+// Mock onLogout function:
 const onLogoutMock = jest.fn();
+
+// Function to get the "Log out" button:
+const getLogOutButton = () => {
+    return screen.getByRole('button', { name: /log out/i });
+}
 
 describe('<Navbar />', () => {
     it('should render correctly', () => {
@@ -38,11 +44,11 @@ describe('<Navbar />', () => {
         })
 
         it('should render "Sign in" option',  () => {
-            expect(screen.getByRole('button', { name: "Sign In" })).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: /sign in/i })).toBeInTheDocument();
         });
 
         it('should render "Sign up" option', () => {
-            expect(screen.getByRole('button', { name: "Sign Up" })).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: /sign up/i })).toBeInTheDocument();
         });
     });
 
@@ -52,11 +58,27 @@ describe('<Navbar />', () => {
                 <Navbar 
                     isUserAuthenticated={true} 
                     onLogout={onLogoutMock} 
-                />);
+                />
+            );
         })
 
-        it('should render "Log out" option', () => {
-            expect(screen.getByRole('button', { name: "Log Out" })).toBeInTheDocument();
+        describe( 'with "Log out" option', () => {
+
+            it('should render "Log out" option', () => {
+                const logOutButton = getLogOutButton();
+
+                expect(logOutButton).toBeInTheDocument();
+            });
+
+            describe('when "Log out" option is clicked', () => {
+              
+                it('should call "onLogout" function', () => {
+                    const logOutButton = getLogOutButton();
+
+                    fireEvent.click(logOutButton);
+                    expect(onLogoutMock).toHaveBeenCalled();
+                });
+            });
         });
     })
 })
